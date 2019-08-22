6e1dc829e34ff8230e8d8a43e28e568c6044f8ef
github username <github email address>
2019-08-22 09:07:07 -0700

    second commit

--- app/controllers/api/v1/questions_controller.rb
+++ app/controllers/api/v1/questions_controller.rb
@@ -4,23 +4,10 @@ class Api::V1::QuestionsController < ApplicationController
         render json: @questions
     end
 
-    def update
-        @question.update(question_params)
-        if @question.save 
-            render json: @question, status: :accepted
-        else
-            render json: { errors: @question.errors.full_messages }, status: :unprocessible_entity
-        end
-    end
-
-    private
-
-    def question_params
-        params.permit(:correct_answer)
-    end
-
-    def find_question
-        @question = Question.find(params[:id])
+    def random
+        @question = Question.all[rand(Question.all.length)]
+        render json: @question
     end
 end
 
+# add functionality if/else if answer is correct/incorrect & routing 
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :questions, only: [:index, :update]
+      resources :questions, only: [:index]
+      resources :users, only: [:index, :show, :new, :create]
+      get '/random', to: 'questions#random'
     end
   end
 end
